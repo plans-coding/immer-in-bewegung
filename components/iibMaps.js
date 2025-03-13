2edb912b112693fe8b1c5d41d27babac63dac08f
plans-coding <152713201+plans-coding@users.noreply.github.com>
2025-03-13 16:09:08 +0100

    Add files via upload

---
+++ components/iibMaps.js
@@ -0,0 +1,286 @@
+        function initializeMapTrip() {
+            const mapContainer = document.getElementById('map');
+            const mapPinDataContainer = document.getElementById('map-pin-data');
+
+            if (mapContainer && mapPinDataContainer) {
+
+            var overviewData = JSON.parse(mapPinDataContainer.getAttribute("data-map-overall-route"));
+            var accommodationData = JSON.parse(mapPinDataContainer.getAttribute("data-map-accommodation"));
+            var assetsSettings = JSON.parse(mapPinDataContainer.getAttribute("data-settings-assets"));
+            var mapColor = mapPinDataContainer.getAttribute("data-map-color");
+
+            //console.log(overviewData);
+            //console.log(accommodationData);
+            //console.log(assetsSettings);
+            //console.log(mapColor);
+
+            const map = L.map('map', {fullscreenControl: true}).setView([39.569176, 2.650108], 10);
+
+            // Base Layer (OpenStreetMap)
+            const baseLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; OpenStreetMap contributors'
+            });
+            baseLayer.addTo(map);
+
+            //map.addControl(new L.Control.Fullscreen());
+
+            const bounds = L.latLngBounds(); // Create bounds object
+
+            // Define Layer Groups
+            const overviewLayer = L.layerGroup();
+            const accommodationLayer = L.layerGroup();
+
+            // Store coordinates for polylines
+            const overviewCoords = [];
+            const accommodationCoords = [];
+
+            // Add Overview Markers & Collect Coordinates
+            overviewData.forEach(location => {
+                const lat = parseFloat(location.Latitude);
+                const lng = parseFloat(location.Longitude);
+
+                const svgIconCar = L.divIcon({
+        className: 'custom-icon',
+        html: `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" class="bi bi-car-front-fill" viewBox="0 0 16 16">  <path fill="`+mapColor+`" d="M2.52 3.515A2.5 2.5 0 0 1 4.82 2h6.362c1 0 1.904.596 2.298 1.515l.792 1.848c.075.175.21.319.38.404.5.25.855.715.965 1.262l.335 1.679q.05.242.049.49v.413c0 .814-.39 1.543-1 1.997V13.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-1.338c-1.292.048-2.745.088-4 .088s-2.708-.04-4-.088V13.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-1.892c-.61-.454-1-1.183-1-1.997v-.413a2.5 2.5 0 0 1 .049-.49l.335-1.68c.11-.546.465-1.012.964-1.261a.8.8 0 0 0 .381-.404l.792-1.848ZM3 10a1 1 0 1 0 0-2 1 1 0 0 0 0 2m10 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2M6 8a1 1 0 0 0 0 2h4a1 1 0 1 0 0-2zM2.906 5.189a.51.51 0 0 0 .497.731c.91-.073 3.35-.17 4.597-.17s3.688.097 4.597.17a.51.51 0 0 0 .497-.731l-.956-1.913A.5.5 0 0 0 11.691 3H4.309a.5.5 0 0 0-.447.276L2.906 5.19Z"/></svg>`,
+        });
+        /*'img/car.svg', iconSize: [25, 25] }) })*/
+
+                if (!isNaN(lat) && !isNaN(lng)) {
+                    L.marker([lat, lng], { icon: svgIconCar })
+                        .bindPopup(`<b>${location.MapPin}</b>`)
+                        .addTo(overviewLayer);
+                    bounds.extend([lat, lng]);
+                    overviewCoords.push([lat, lng]);
+                }
+            });
+
+            // Add Accommodation Markers & Collect Coordinates
+            accommodationData.forEach(accommodation => {
+                if (accommodation.AccommodationCoordinates) {
+                    const coords = accommodation.AccommodationCoordinates.split(',').map(parseFloat);
+
+                    const svgIcon = L.divIcon({
+        className: 'custom-icon',
+        html: `<svg class="iib-trip-tab" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"	 width="32px" height="32px" viewBox="0 0 460.298 460.297" style="enable-background:new 0 0 460.298 460.297;"	 xml:space="preserve"><g>	<g>		<path fill="#6f757e" d="M230.149,120.939L65.986,256.274c0,0.191-0.048,0.472-0.144,0.855c-0.094,0.38-0.144,0.656-0.144,0.852v137.041			c0,4.948,1.809,9.236,5.426,12.847c3.616,3.613,7.898,5.431,12.847,5.431h109.63V303.664h73.097v109.64h109.629			c4.948,0,9.236-1.814,12.847-5.435c3.617-3.607,5.432-7.898,5.432-12.847V257.981c0-0.76-0.104-1.334-0.288-1.707L230.149,120.939			z"/>		<path fill="#6f757e" d="M457.122,225.438L394.6,173.476V56.989c0-2.663-0.856-4.853-2.574-6.567c-1.704-1.712-3.894-2.568-6.563-2.568h-54.816			c-2.666,0-4.855,0.856-6.57,2.568c-1.711,1.714-2.566,3.905-2.566,6.567v55.673l-69.662-58.245			c-6.084-4.949-13.318-7.423-21.694-7.423c-8.375,0-15.608,2.474-21.698,7.423L3.172,225.438c-1.903,1.52-2.946,3.566-3.14,6.136			c-0.193,2.568,0.472,4.811,1.997,6.713l17.701,21.128c1.525,1.712,3.521,2.759,5.996,3.142c2.285,0.192,4.57-0.476,6.855-1.998			L230.149,95.817l197.57,164.741c1.526,1.328,3.521,1.991,5.996,1.991h0.858c2.471-0.376,4.463-1.43,5.996-3.138l17.703-21.125			c1.522-1.906,2.189-4.145,1.991-6.716C460.068,229.007,459.021,226.961,457.122,225.438z"/>	</g></g></svg>`,
+        });
+
+                    if (coords.length === 2 && !isNaN(coords[0]) && !isNaN(coords[1])) {
+                        L.marker(coords, { icon: svgIcon }) /*L.icon({ iconUrl: 'img/house.svg', iconSize: [25, 25] })*/
+                            .bindPopup(`<b>${accommodation.Accommodation}</b><br>${accommodation.Date}<br>${accommodation.AccommodationCoordinates} ${accommodation.AccommodationCoordinatesAccuracy ? `Accuracy: ${accommodation.AccommodationCoordinatesAccuracy}` : ''}`)
+                            .addTo(accommodationLayer);
+                        bounds.extend(coords);
+                        accommodationCoords.push(coords);
+                    }
+                }
+            });
+
+            // Function to add polylines with arrows
+            function addPolylineDecorators(layer, coords, color) {
+                if (coords.length > 1) {
+                    const polyline = L.polyline(coords, { color: color, weight: 5 }).addTo(layer);
+
+                    const decorator = L.polylineDecorator(polyline, {
+                        patterns: [
+                            {
+                                offset: '50%',
+                                symbol: L.Symbol.arrowHead({
+                                    pixelSize: 15,
+                                    headAngle: 30,
+                                    polygon: false,
+                                    pathOptions: { stroke: true, color: color }
+                                })
+                            }
+                        ]
+                    }).addTo(layer);
+                }
+            }
+
+            // Add polylines with arrows
+            addPolylineDecorators(overviewLayer, overviewCoords, mapColor);
+            addPolylineDecorators(accommodationLayer, accommodationCoords, '#6f757e');
+
+            // Add Layer Control
+            L.control.layers(null, {
+                "Overview": overviewLayer,
+                "Accommodation": accommodationLayer
+            }, {collapsed: false}).addTo(map);
+
+            // Add layers to the map
+            overviewLayer.addTo(map);
+            accommodationLayer.addTo(map);
+
+            // Fit map to bounds if there are valid markers
+
+            map.fitBounds(bounds);
+
+            } else {
+                setTimeout(initializeMapTrip, 100);
+            }
+        }
+
+        function initializeMapContours() {
+            const mapContainer = document.getElementById('map');
+            const mapPinDataContainer = document.getElementById('map-pin-data');
+
+            if (mapContainer && mapPinDataContainer) {
+                const map = L.map('map', {fullscreenControl: true}).setView([51.505, -0.09], 6);
+
+                L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                    attribution: '&copy; OpenStreetMap contributors'
+                }).addTo(map);
+
+                try {
+                    const rawData = mapPinDataContainer.getAttribute('data-map');
+                    const jsonData = JSON.parse(rawData);
+
+                    /* --- BIND INDIVIDUAL TRIP POINTS
+                    const bounds = L.latLngBounds(); // Create bounds object
+
+                    jsonData.forEach(pin => {
+                        const { MergedAccommodationCoordinates } = pin;
+
+                        // Split the string into individual coordinate pairs
+                        const coordinates = MergedAccommodationCoordinates.split('|').map(coord => {
+                            const [lat, lon] = coord.split(',').map(parseFloat); // Parse latitude and longitude
+
+                            // Validate if both lat and lon are valid numbers
+                            if (isNaN(lat) || isNaN(lon)) {
+                                console.warn(`Invalid coordinates: ${coord}`);
+                                return null; // Skip invalid coordinates
+                            }
+
+                            return [lat, lon]; // Return as an array of [lat, lon]
+                        }).filter(coord => coord !== null); // Remove invalid coordinates
+
+                        // Create a polyline from the valid array of coordinates
+                        if (coordinates.length > 0) {
+                            const polyline = L.polyline(coordinates, {
+                                color: '#1d655e', // Set the polyline color
+                                weight: 3,             // Set the line thickness (optional)
+                                opacity: 0.7          // Set the opacity (optional)
+                                })
+                                .addTo(map)
+                                .bindPopup(`<b>${pin.InnerId}</b>`)
+                                .on('click', function () {
+                                    this.openPopup();
+                                });
+
+                            bounds.extend(polyline.getBounds()); // Extend bounds to fit the polyline
+
+
+
+                                } else {
+                                    console.warn(`Skipping polyline for ${pin.InnerId} due to invalid coordinates.`);
+                                }
+                            });
+
+                            if (jsonData.length > 0) {
+                                map.fitBounds(bounds, { padding: [50, 50] }); // Auto-zoom to fit all the polylines
+                            }
+
+                            console.log("Map initialized with auto-zoom");
+                            --- BIND INDIVIDUAL TRIP POINTS */
+
+                    // BIND ALL TRIP ACCOMMODATION POINTS TOGEHTER
+                    const allCoordinates = [];
+
+                    jsonData.forEach(pin => {
+                        const { MergedAccommodationCoordinates } = pin;
+
+                        // Split the string into individual coordinate pairs
+                        const coordinates = MergedAccommodationCoordinates.split('|').map(coord => {
+                            const [lat, lon] = coord.split(',').map(parseFloat); // Parse latitude and longitude
+
+                            // Validate if both lat and lon are valid numbers
+                            if (isNaN(lat) || isNaN(lon)) {
+                                console.warn(`Invalid coordinates: ${coord}`);
+                                return null; // Skip invalid coordinates
+                            }
+
+                            return [lat, lon]; // Return as an array of [lat, lon]
+                        }).filter(coord => coord !== null); // Remove invalid coordinates
+
+                        // Append valid coordinates to the global array
+                        allCoordinates.push(...coordinates);
+                    });
+
+                    // Create a single polyline from all coordinates
+                    if (allCoordinates.length > 0) {
+                        const polyline = L.polyline(allCoordinates, {
+                            color: '#1d655e', // Set the polyline color
+                            weight: 3,        // Set the line thickness (optional)
+                            opacity: 0.7      // Set the opacity (optional)
+                        }).addTo(map);
+
+                        map.fitBounds(polyline.getBounds()); // Adjust map to fit the polyline
+                    }
+                    // BIND ALL TRIP ACCOMMODATION POINTS TOGEHTER
+
+
+                } catch (error) {
+                    console.error("JSON Parse Error:", error);
+                }
+            } else {
+                setTimeout(initializeMapContours, 100);
+            }
+        }
+
+        function initializeMapCountry() {
+            const mapContainer = document.getElementById('map');
+            const mapPinDataContainer = document.getElementById('map-pin-data');
+
+            if (mapContainer && mapPinDataContainer) {
+                const map = L.map('map', {fullscreenControl: true}).setView([51.505, -0.09], 6);
+
+                L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                    attribution: '&copy; OpenStreetMap contributors'
+                }).addTo(map);
+
+                try {
+                    const rawData = mapPinDataContainer.getAttribute('data-map');
+                    const jsonData = JSON.parse(rawData);
+                    const bounds = L.latLngBounds(); // Create bounds object
+
+                    jsonData.forEach(pin => {
+                        const { InnerId, OuterId, OverallDestination, AccommodationCoordinates, AccommodationCoordinatesAccuracy, Accommodation, ParticipantGroup, TravelParticipants, Date, CountriesDuringDay } = pin;
+
+                        // Split coordinates from the 'AccommodationCoordinates' field
+                        const [lat, lon] = AccommodationCoordinates.split(',').map(coord => parseFloat(coord.trim()));
+
+                        // Popup content with conditional rendering for accuracy
+                        let popupContent = `
+                            <b>${Accommodation}</b> <br>
+                            ${ParticipantGroup} ${TravelParticipants} <br>
+                            ${Date} <br>
+                            <a href="?p=trip&id=${OuterId}"><div class="iib-map-ref" data-iib-tripDomain="${InnerId.charAt(0)}">${OuterId} ${OverallDestination}</div></a> <br>
+                        `;
+
+                        // Add accuracy only if it's not null
+                        if (AccommodationCoordinatesAccuracy) {
+                            popupContent += `<b>Coordinate Accuracy:</b> ${AccommodationCoordinatesAccuracy} <br>`;
+                        }
+
+                        const marker = L.marker([lat, lon], { icon: L.icon({ iconUrl: 'img/house.svg', iconSize: [25, 25] }) })
+                            .addTo(map)
+                            .bindPopup(popupContent)
+                            .on('click', function () {
+                                this.openPopup();
+                            });
+
+                        bounds.extend([lat, lon]); // Extend bounds for each marker
+                    });
+
+                    if (jsonData.length > 0) {
+                        map.fitBounds(bounds, { padding: [50, 50] }); // Auto-zoom to markers
+                    }
+
+                    console.log("Map initialized with auto-zoom");
+
+
+                } catch (error) {
+                    console.error("JSON Parse Error:", error);
+                }
+            } else {
+                setTimeout(initializeMapCountry, 100);
+            }
+        }
---
+++ components/iibQueries.js
@@ -0,0 +1,308 @@
+function getSqlQuery(section, parameter = null) {
+
+    switch (section) {
+
+        // COMMON
+        case "common_tripDomains":
+            return `SELECT
+                    *
+                FROM
+                    bewx_TripDomains;`;
+        break;
+
+        // OVERVIEW
+        case "overview_year":
+               return `SELECT
+                    *,
+                    SUBSTR(InnerId, 1, 1) AS TripDomain,
+                    CAST(strftime('%Y', DepartureDate) AS INTEGER) AS TripYear,
+                    (
+                        CAST(strftime('%Y', DepartureDate) AS INTEGER) / 10
+                    )
+                    * 10 AS TripDecade
+                FROM
+                    bewa_Overview
+                WHERE
+                    InnerId IS NOT NULL
+                ORDER BY
+                    DepartureDate DESC;`
+        break;
+
+        case "overview_country":
+            return `SELECT
+                    bewx_ContinentCountries.Continent,
+                    bewx_ContinentCountries.Country,
+                    IIBc_BorderCrossings.OuterId,
+                    IIBc_BorderCrossings.InnerId,
+                    bewa_Overview.OverallDestination
+                FROM
+                    bewx_ContinentCountries
+                JOIN
+                    IIBc_BorderCrossings
+                    ON IIBc_BorderCrossings.AllBorderCrossings LIKE '%' || bewx_ContinentCountries.Country || '%'
+                JOIN
+                    bewa_Overview
+                    ON IIBc_BorderCrossings.InnerId = bewa_Overview.InnerId
+                ORDER BY
+                CASE
+                    WHEN
+                        bewx_ContinentCountries.Continent = 'Europa'
+                    THEN
+                        0
+                    ELSE
+                        1
+                END
+                , bewx_ContinentCountries.Continent ASC, bewx_ContinentCountries.Country ASC;`;
+        break;
+
+        // TRIP
+        case "trip_summary":
+            return `SELECT
+                    *,
+                    SUBSTR(InnerId, 1, 1) AS DomainAbbreviation
+                FROM
+                    bewa_Overview
+                WHERE
+                    OuterId = "${parameter}"
+                LIMIT 1;`;
+        break;
+
+        case "trip_events":
+            return `SELECT
+                    *
+                FROM
+                    IIBb_Events
+                WHERE
+                    OuterId = "${parameter}"
+                ORDER BY
+                    Date;`;
+        break;
+
+        case "trip_allTrips":
+            return `SELECT
+                    SUBSTR(InnerId, 1, 1) AS DomainAbbreviation,
+                    OuterId,
+                    OverallDestination
+                FROM
+                    bewa_Overview
+                WHERE
+                    OuterId IS NOT NULL
+                ORDER BY
+                    DepartureDate ASC;`;
+        break;
+
+        case "trip_borderCrossings":
+            return `SELECT
+                    *
+                FROM
+                    IIBc_BorderCrossings
+                WHERE
+                    OuterId = "${parameter}";`;
+        break;
+
+        case "trip_mapPins":
+            return `WITH RECURSIVE SplitPins AS
+                (
+                -- Start by selecting the first portion of the string
+                SELECT
+                    OuterId,
+                    SUBSTR(MapPins, INSTR(MapPins, '{') + 2, INSTR(MapPins, '}') - INSTR(MapPins, '{') - 2) AS PinEntry,
+                    SUBSTR(MapPins, INSTR(MapPins, '}') + 2) AS Remaining
+                FROM
+                    bewa_Overview
+                WHERE
+                    OuterId = "${parameter}"
+                UNION ALL
+                -- Recursively extract subsequent portions
+                SELECT
+                    OuterId,
+                    SUBSTR(Remaining, INSTR(Remaining, '{') + 2, INSTR(Remaining, '}') - INSTR(Remaining, '{') - 2) AS PinEntry,
+                    SUBSTR(Remaining, INSTR(Remaining, '}') + 2)
+                FROM
+                    SplitPins
+                WHERE
+                    Remaining LIKE '%{%'
+                )
+                -- Extract name and coordinates from each pin entry
+                SELECT
+                "${parameter}" AS OuterId,
+                TRIM(SUBSTR(PinEntry, 1, INSTR(PinEntry, '|') - 1)) AS MapPin,
+                TRIM(SUBSTR(PinEntry, INSTR(PinEntry, '|') + 2, INSTR(PinEntry, ',') - INSTR(PinEntry, '|') - 2)) AS Latitude,
+                TRIM(SUBSTR(PinEntry, INSTR(PinEntry, ',') + 2)) AS Longitude
+                FROM
+                SplitPins;`;
+        break;
+
+        // MAP
+        case "map_countryList":
+            return `SELECT
+                    DISTINCT AccommodationCountry
+                FROM
+                    IIBb_Events
+                WHERE
+                    AccommodationCountry NOT LIKE '(%'
+                    AND AccommodationCountry NOT LIKE '-%'
+                ORDER BY
+                    AccommodationCountry;`;
+        break;
+
+        case "map_contour":
+            return `SELECT
+                        InnerId,
+                        GROUP_CONCAT(AccommodationCoordinates, '|') AS MergedAccommodationCoordinates
+                    FROM
+                        bewb_Events
+                    WHERE
+                        AccommodationCoordinates IS NOT NULL
+                    GROUP BY
+                        InnerId;`;
+        break;
+
+        case "map_country":
+            return `SELECT
+                        * FROM IIBb_Events
+                    WHERE
+                        AccommodationCountry = "${parameter}";`;
+        break;
+
+        // STATISTICS
+        case "statistics_perDomainYear":
+            return `SELECT
+                    SUBSTR(InnerId, 1, 1) AS DomainAbbreviation,
+                    SUBSTR(DepartureDate, 1, 4) AS Year,
+                    COUNT(*) AS AbbreviationCount
+                FROM
+                    bewa_Overview
+                WHERE
+                    InnerId IS NOT NULL
+                    AND DepartureDate IS NOT NULL
+                GROUP BY
+                    DomainAbbreviation, Year
+                ORDER BY
+                    Year ASC, AbbreviationCount DESC;`;
+        break;
+
+        case "statistics_tripCount":
+            return `SELECT
+                    COUNT(InnerId) As Count
+                 FROM
+                    bewa_Overview;`;
+        break;
+
+        case "statistics_overnights":
+            return `SELECT
+                    AccommodationCountry, COUNT(*) AS Overnights
+                FROM
+                    bewb_Events
+                GROUP BY
+                    AccommodationCountry
+                ORDER BY
+                    Overnights DESC;`;
+        break;
+
+        case "statistics_OLSSVSS":
+            return `WITH SplitCountries AS (
+                    SELECT
+                        OuterId,
+                        InnerId,
+                        TRIM(value) AS RawCountry
+                    FROM IIBc_BorderCrossings, json_each('["' || REPLACE(AllBorderCrossings, ', ', '","') || '"]')
+                ),
+                Normalized AS (
+                    SELECT DISTINCT
+                        OuterId,
+                        InnerId,
+                        REPLACE(REPLACE(REPLACE(RawCountry, '*', ''), '**', ''), '+', '') AS Country,
+                        RawCountry
+                    FROM SplitCountries
+                ),
+                CountRaw AS (
+                    SELECT
+                        REPLACE(REPLACE(REPLACE(TRIM(value), '*', ''), '**', ''), '+', '') AS Country,
+                        COUNT(*) AS OLMQ
+                    FROM IIBc_BorderCrossings, json_each('["' || REPLACE(AllBorderCrossings, ', ', '","') || '"]')
+                    WHERE TRIM(value) NOT LIKE '*%'
+                    AND TRIM(value) NOT LIKE '**%'
+                    AND TRIM(value) NOT LIKE '+%'
+                    GROUP BY Country
+                ),
+                CountPrefixed AS (
+                    SELECT
+                        REPLACE(REPLACE(REPLACE(TRIM(value), '*', ''), '**', ''), '+', '') AS Country,
+                        SUM(CASE WHEN TRIM(value) LIKE '*%' THEN 1 ELSE 0 END) AS SSMQ,
+                        SUM(CASE WHEN TRIM(value) LIKE '**%' THEN 1 ELSE 0 END) AS VSSMQ,
+                        SUM(CASE WHEN TRIM(value) LIKE '+%' THEN 1 ELSE 0 END) AS PSMQ
+                    FROM IIBc_BorderCrossings, json_each('["' || REPLACE(AllBorderCrossings, ', ', '","') || '"]')
+                    GROUP BY Country
+                ),
+                Aggregated AS (
+                    SELECT
+                        n.Country,
+                        COUNT(*) AS OL,
+                        cr.OLMQ AS OLMQ,
+                        SUM(CASE WHEN n.RawCountry LIKE '*%' THEN 1 ELSE 0 END) AS SS,
+                        SUM(CASE WHEN n.RawCountry LIKE '**%' THEN 1 ELSE 0 END) AS VSS,
+                        SUM(CASE WHEN n.RawCountry LIKE '+%' THEN 1 ELSE 0 END) AS PS,
+                        cp.SSMQ,
+                        cp.VSSMQ,
+                        cp.PSMQ
+                    FROM Normalized n
+                    LEFT JOIN CountRaw cr ON n.Country = cr.Country
+                    LEFT JOIN CountPrefixed cp ON n.Country = cp.Country
+                    GROUP BY n.Country
+                )
+                SELECT
+                    Country,
+                    OL,
+                    SS,
+                    VSS,
+                    PS,
+                    OLMQ,
+                    SSMQ,
+                    VSSMQ,
+                    PSMQ
+                FROM Aggregated
+                ORDER BY OL DESC;`;
+        break;
+
+        // SEARCH
+        case "search_trip":
+            return `SELECT
+                    'TripQuery' AS Query,
+                    *
+                FROM
+                    bewa_Overview
+                RIGHT JOIN IIBc_BorderCrossings USING (InnerId, OuterId)
+                WHERE
+                (
+                    COALESCE(ParticipantGroup, '') || ' ' || COALESCE(OverallDestination, '') || ' ' || COALESCE(DepartureDate, '') || ' ' || COALESCE(ReturnDate, '') || ' ' || COALESCE(MapPins, '') || ' ' || COALESCE(TripDescription, '') || ' ' || COALESCE(AllBorderCrossings, '')
+                ) LIKE '%${parameter}%';`;
+        break;
+
+        case "search_event":
+            return `SELECT
+                    'EventQuery' AS Query,
+                    *
+                FROM
+                    IIBb_Events
+                WHERE
+                (
+                    COALESCE(OuterId, '') || ' ' || COALESCE(OverallDestination, '') || ' ' || COALESCE(Date, '') || ' ' || COALESCE(Events, '') || ' ' || COALESCE(Accommodation, '') || ' ' || COALESCE(AccommodationCountry, '') || ' ' || COALESCE(ParticipantGroup, '') || ' ' || COALESCE(TravelParticipants, '') || ' ' || COALESCE(AdditionalNotes, '') || ' ' || COALESCE(CountriesDuringDay, '')
+                ) LIKE '%${parameter}%';`;
+        break;
+
+        // IMAGES
+        case "images_dateList":
+            return `SELECT
+                    SUBSTR(InnerId, 1, 1) AS DomainAbbreviation,
+                    Date
+                FROM
+                    IIBb_Events
+                WHERE
+                    OuterId = "${parameter}"
+                ORDER BY
+                    Date ASC;`;
+        break;
+
+                    }
+    }
